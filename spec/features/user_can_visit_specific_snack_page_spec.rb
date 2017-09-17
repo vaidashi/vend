a98d5fb6e280363abb413e53ed4504e8b8c14fe3
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-17 09:02:29 -0600

    machine locations for snack feature

--- spec/features/user_can_visit_specific_snack_page_spec.rb
+++ spec/features/user_can_visit_specific_snack_page_spec.rb
@@ -10,10 +10,21 @@ feature "When a user visits a snack show page" do
     expect(page).to have_content(snack.price)
   end
 
-  # scenario "I see a list of locations with vending machines that carry that snack" do
-  #
-  #
-  # end
+  scenario "I see a list of locations with vending machines that carry that snack" do
+    snack = Snack.create(name: "Flaming Hot Cheetos", price: 2.50)
+    owner = Owner.create(name: "Sam's Snacks")
+
+    dons   = owner.machines.create(location: "Don's Mixed Drinks")
+    turing = owner.machines.create(location: "Turing Basement")
+
+    dons.snacks << snack
+    turing.snacks << snack
+
+    visit snack_path(snack)
+
+    expect(page).to have_content("Don's Mixed Drinks")
+    expect(page).to have_content("Turing Basement")
+  end
   #
   # scenario "I see the average price for snacks in those vending machines" do
   #
