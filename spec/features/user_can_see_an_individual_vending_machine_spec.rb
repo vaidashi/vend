eb6feefef97c0d8507e3b49be4ba5611bf25aed7
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-09-17 08:27:08 -0600

    first feature passing, created snack and machine snack join table

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -9,4 +9,17 @@ feature 'When a user visits a vending machine show page' do
 
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
+
+  scenario 'I see the name of all of the snacks associated with that vending machine along with their price' do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack = Snack.create(name: "Doritos", price: 1.0)
+    dons.snacks << snack
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Don's Mixed Drinks Vending Machine")
+    expect(page).to have_content("Doritos")
+    expect(page).to have_content(1.0)
+  end
 end
